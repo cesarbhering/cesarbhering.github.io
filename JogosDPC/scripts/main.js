61b194f33b93443444cc3fe08dff259568741b5c
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-04-14 11:49:16 -0300

    Adiciona countdown em tempo real e css melhorado

--- JogosDPC/scripts/main.js
+++ JogosDPC/scripts/main.js
@@ -1,16 +1,14 @@
 const myJSON = JSON.parse(upcoming);
 const divGames =  document.getElementById('listAllGames');
 
-//FALTA FAZER O COUNTDOWN SER EM TEMPO REAL.
-//VER ERRO CHATO NA LINHA 42, PQP.
-//FALTA ESTILIZAR MELHOR A DATA APRESENTADA
-//FALTA ESTILIZAR MELHOR O COUNTDOWN.
+
 //FALTA VER COMO ATUALIZAR O JSON SERVER SIDE - BUSCAR MAIS INFOS.
 //PEGAR UNS LAYOUTS MANEIROS PARA OS CARDS
-//DEPOIS DE TERMINAR O DA SG TENTAR FAZER O DO RDO DEPOIS DUNHA1-DEPOIS NOPING
+//DEPOIS DE TERMINAR O DA SG TENTAR FAZER O DO RDO DEPOIS DUNHA1-DEPOIS NOPING - DEPOIS CREWMATES - DEPOIS BINOMISTAS
 // PALETA: https://coolors.co/03045e-023e8a-0077b6-0096c7-00b4d8-48cae4-90e0ef-ade8f4-caf0f8
+// Adicionar as transmissoes (em PT BR CLARO)
 
-
+// Cria os cartões dos jogos
 function createGamesCard() {
 	for (let games in myJSON) {
 		let game = document.createElement('div');
@@ -20,6 +18,7 @@ function createGamesCard() {
 }
 createGamesCard();
 
+// Função que calcula dias,horas,minutos,segundos para os jogos.
 function countDownClock(gameDate) {
 	let countDownDate = new Date(gameDate).getTime()
 	let dateTimeNow = new Date().getTime();
@@ -28,26 +27,40 @@ function countDownClock(gameDate) {
 	let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
 	let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
 	let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
-	return `Faltam Dias: ${days} - ${hours}:${minutes}:${seconds}`;
+	if (days > 0) {
+		return `Faltam ${days} dias ${hours}hrs ${minutes}mins ${seconds}segs`;
+	} else if (days === 0 && hours > 0) {
+		return `Faltam ${hours}hrs ${minutes}mins ${seconds}segs`
+	} else if (days === 0 && hours === 0) {
+		return `Faltam ${minutes}mins ${seconds}segs`
+	}
 };
 
-
-let novadata = myJSON[0].begin_at;
-console.log(novadata)
-
+// Coloca as infos nos cartões dos jogos
 function populateGameCard() {
 	let allGamesCards = document.querySelectorAll('.gameCard');
-	for (let card in allGamesCards) {
+	for (let index = 0; index < myJSON.length; index += 1) {
 		let gameName = document.createElement('p');
 		let gameDate = document.createElement('p');
 		let gameCountDown = document.createElement('p');
-		gameName.innerText = myJSON[card].name;
-		gameDate.innerText = 'Agendado Para: ' + myJSON[card].begin_at.slice(0,10) + ' às ' + myJSON[card].begin_at.slice(11,16);
-		gameCountDown.innerText = countDownClock(myJSON[card].begin_at);
-		allGamesCards[card].appendChild(gameName);
-		allGamesCards[card].appendChild(gameCountDown);
-	  allGamesCards[card].appendChild(gameDate);
+		gameName.innerText = myJSON[index].name;
+		gameDate.innerText = 'Agendado Para: ' + myJSON[index].begin_at.slice(0,10) + ' às ' + myJSON[index].begin_at.slice(11,16);
+		gameCountDown.innerText = countDownClock(myJSON[index].begin_at);
+		gameCountDown.setAttribute('id', 'countdown');
+		allGamesCards[index].appendChild(gameName);
+		allGamesCards[index].appendChild(gameCountDown);
+	  allGamesCards[index].appendChild(gameDate);
 	}
 }
 
 populateGameCard();
+
+// Atualiza o quanto falta para os jogos a cada 1 segundo.
+function refreshCountDown() {
+	let allGamesCountDown = document.querySelectorAll('#countdown');
+	for (let index = 0; index < allGamesCountDown.length; index += 1) {
+		allGamesCountDown[index].innerText = countDownClock(myJSON[index].begin_at);
+	}
+}
+
+setInterval(refreshCountDown, 1000)
